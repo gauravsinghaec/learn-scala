73e96594c6bd220348582c01a15dc9caf157ace3
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-07-30 17:27:49 +0530

    code: add inheretence

--- src/prac2.scala
+++ src/prac2.scala
@@ -1,10 +1,20 @@
-class Point(a: Int, b: Int){
+class Point(val a: Int, val b: Int){
   var x = a
   var y = b
 
   def move(dx: Int, dy:Int){
     x = x + dx
     y = y + dy
+    println("Here is the new point " +(x, y))
+  }
+}
+class Location(override val a: Int, override val b: Int, val c: Int) extends Point(a,b){
+  var z = c
+  def move(dx: Int, dy: Int, dz: Int){
+    x = x + dx
+    y = y + dy   
+    z = z + dz
+    println("The new location is "+(x,y,z))   
   }
 }
 
@@ -12,6 +22,7 @@ object Demo {
   def main(args: Array[String]){
     val p = new Point(5,6)
     p.move(4,4)
-    println("Here is the new point " +(p.x, p.y))
+    val l = new Location(6, 9, 23)
+    l.move(4,4,3)
   }
 }
