5d4485506b6e0b7d9c85e03325f7d6c6d9f1ca7b
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-07-31 15:17:14 +0530

    functions: normal + HOC, Lambda, Currying

--- src/functions.scala
+++ src/functions.scala
@@ -12,6 +12,25 @@ object Demo{
     println(s"Calling func3(4) a+b = ${func3(4)}")
 
     println(s"Calling func3(b=6) with named param a+b = ${func3(b=6)}")
+
+    println("\n-------HOF-------")
+    println(s"hof(23, increaseBy10) = ${hof(23, increaseBy10)}")
+
+    println("\n-------HOF Composition-------")
+    println(s"increaseBy10(multiplyBy2(11)) = ${increaseBy10(multiplyBy2(11))}")
+
+    println("\n-------HOF Currying 1-------")
+    println(s"hofCurrying(4)(11) = ${hofCurrying(4)(11)}")
+    println("\n-------HOF Currying 2-------")
+    var newF = hofCurrying(34)_
+    println(s"newF(7)=hofCurrying(34)_ = ${newF(7)}")
+    
+    println("\n-------Anonymous/Lambda functions-------")
+    var result1 = (_: Int)+(_: Int)
+    var result2 = (a: Int,b: Int) => a+b
+    println(s"The result of result1=(_: Int)+(_: Int) = ${result1(34,2)}")
+    println(s"The result of result2=(a: Int,b: Int) => a+b = ${result2(34,2)}")
+
   }
   // Function without = operator
   def func1(a: Int, b: Int){
@@ -23,5 +42,17 @@ object Demo{
   }
   def func3(a: Int=100, b: Int=100)={
     a+b
+  }
+  def hof(a: Int, f: Int=> Int) ={
+    f(a)
+  }
+  def increaseBy10(n: Int) = {
+    n+10
+  }
+  def multiplyBy2(m: Int) = {
+    m*2
+  }
+  def hofCurrying(m: Int)(n: Int) = {
+    m+n
   }
 }
