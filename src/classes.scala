d832f967c88d99eb2fd7204d9b7e531b361ca949
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-08-01 22:07:17 +0530

    class: primary constructor

--- src/classes.scala
+++ src/classes.scala
@@ -8,9 +8,21 @@ class Vehicle{
   }
 }
 
+class VehiclePrimCons(positionX:Int, positionY: Int){
+  def location() = {
+    (positionX,positionY)
+  }
+}
+
 object Demo{
   def main(args:Array[String]){
     val Car = new Vehicle()
     println(Car.vehicleType)
+
+    println("\n-------- Primary Constructor--------")
+    val van1 = new VehiclePrimCons(10, 30)
+    val van2 = new VehiclePrimCons(2,3)
+    println(van1.location)
+    println(van2.location)
   }
 }
