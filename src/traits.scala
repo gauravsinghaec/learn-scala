06d5c1b2d9153bfbca39dd38ee9996351795117d
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-08-06 10:13:42 +0530

    code: add trait mixin examples

--- src/traits.scala
+++ src/traits.scala
@@ -6,6 +6,18 @@ trait Language {
   def language() 
 }
 
+abstract class Home{
+  val name : String
+  def puja()
+}
+
+class Investments(val c: Int) {
+  val count : Int = c
+  def mf(){
+    println("I do invest in MF")
+  }
+}
+
 // This will give error because if you don't implement the trails then the class must be declared as abstract
 /*
 class Gaurav extends Speak with Language{
@@ -31,11 +43,60 @@ class Anuja extends Speak with Language{
   }
 }
 
+// ******* Below code will not compile as it break the scala traits mixin rule ******* //
+/*
+class NotPerfect extends Speak with Home with Investments{
+  def speak(){
+    println("I can speak multi languages")
+  }
+}
+
+class AgainNotPerfect extends Home with Speak with Investments{
+  def speak(){
+    println("I can speak multi languages")
+  }
+}
+*/
+
+// First abstract class then traits
+class Perfect1(override val c: Int) extends Investments(c) with Speak with Language{
+  override val count = c
+  def speak(){
+    println("I can speak in perfect 1")
+  }
+  def language(){
+    println("I know three languages")
+  }
+}
+
+// First class then traits
+class Perfect2 extends Home with Speak with Language{
+  override val name = "Golden Home"
+  def speak(){
+    println("I can speak in perfect 2")
+  }
+  def language(){
+    println("I know two languages")
+  }
+  def puja(){
+    println("I will perform house pooja")
+  }
+}
+
+
 object Demo {
   def main(args: Array[String]){
     val anu = new Anuja()
     anu.speak
     anu.language
+    println("********* Scala Trait Mixin examples *********")
+    val p1 = new Perfect1(16)
+    p1.speak
+    println(p1.count)
+    val p2 = new Perfect2()
+    p2.speak
+    println(p2.name)
+    p2.puja
   }
 }
 
