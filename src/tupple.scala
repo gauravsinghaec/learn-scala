2552bf251929e54a10d77fc50baf221af4474f9d
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-08-06 11:32:55 +0530

    code: accessing tupple values and pattern matching

--- src/tupple.scala
+++ src/tupple.scala
@@ -7,6 +7,26 @@ object Demo {
     println(tupple1)
     println(tupple2)
     println(tupple3)
-    
+    println("\n************* Iterating Tupples using 'productIterator' ************")
+    tupple3.productIterator.foreach(println)
+
+    println("\n************* Accessing Tupple values ************")
+    println(s"tupple2._1 = ${tupple2._1}")
+    println(s"tupple2._3 = ${tupple2._3}")
+
+    println("\n************* Pattern Matching in Tupple ************")
+    val ingredient = ("Sugar", 25)
+    val (name, quantity) = ingredient
+    println(s"name = ${name}")
+    println(s"quantity = ${quantity}")
+
+    val planets =
+      List(("Mercury", 57.9), ("Venus", 108.2), ("Earth", 149.6),
+          ("Mars", 227.9), ("Jupiter", 778.3))
+    planets.foreach {
+      case ("Earth", distance) =>
+        println(s"Our planet is $distance million kilometers from the sun")
+      case _ =>
+    }
   }
 }
