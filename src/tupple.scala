69366976a36035bc54d6e4ec0d8ec3e5061547d7
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-08-06 11:06:20 +0530

    code: basic tupples

---
+++ src/tupple.scala
@@ -0,0 +1,12 @@
+object Demo {
+  def main(args: Array[String]){
+    val tupple1 = (1,2,4,5,8)
+    val tupple2 = ("Anuja", 22, "Gaurav", 1, 4, 9)
+    val tupple3 = (2.8, 22, 15.6, 1, 4, 9)
+    println("\n************* Basic Tupples ************")
+    println(tupple1)
+    println(tupple2)
+    println(tupple3)
+    
+  }
+}
